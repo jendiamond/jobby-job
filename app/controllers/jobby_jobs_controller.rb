b1e8a1f75019dc45d668343bd202739fc51348be
jendiamond <thejendiamond@gmail.com>
2016-03-26 02:19:28 -0700

    Add skeleton for styling
    On branch master
    Changes to be committed:
    modified:   app/assets/stylesheets/application.css
    new file:   app/assets/stylesheets/normalize.css
    new file:   app/assets/stylesheets/skeleton.css
    modified:   app/controllers/jobby_jobs_controller.rb
    modified:   app/views/jobby_jobs/_form.html.erb
    modified:   app/views/jobby_jobs/edit.html.erb
    modified:   app/views/jobby_jobs/index.html.erb
    modified:   app/views/jobby_jobs/new.html.erb
    modified:   app/views/jobby_jobs/show.html.erb
    modified:   app/views/layouts/application.html.erb

--- app/controllers/jobby_jobs_controller.rb
+++ app/controllers/jobby_jobs_controller.rb
@@ -17,36 +17,26 @@ class JobbyJobsController < ApplicationController
 
   def create
     @jobby_job = JobbyJob.new(jobby_job_params)
-
-    respond_to do |format|
-      if @jobby_job.save
-        format.html { redirect_to @jobby_job, notice: 'Jobby job was successfully created.' }
-        format.json { render :show, status: :created, location: @jobby_job }
-      else
-        format.html { render :new }
-        format.json { render json: @jobby_job.errors, status: :unprocessable_entity }
-      end
+    if @jobby_job.save
+      redirect_to @jobby_job
+    else
+      render :new
     end
   end
 
   def update
-    respond_to do |format|
-      if @jobby_job.update(jobby_job_params)
-        format.html { redirect_to @jobby_job, notice: 'Jobby job was successfully updated.' }
-        format.json { render :show, status: :ok, location: @jobby_job }
-      else
-        format.html { render :edit }
-        format.json { render json: @jobby_job.errors, status: :unprocessable_entity }
-      end
+    if @jobby_job.update(jobby_job_params)
+      redirect_to '/'
+    else
+      render :edit
+      flash[:notice] = "Workout was not edited."
     end
   end
 
   def destroy
     @jobby_job.destroy
-    respond_to do |format|
-      format.html { redirect_to jobby_jobs_url, notice: 'Jobby job was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    flash[:notice] = "Workout deleted."
+    redirect_to '/'
   end
 
   private
@@ -55,6 +45,9 @@ class JobbyJobsController < ApplicationController
     end
 
     def jobby_job_params
-      params.require(:jobby_job).permit(:company, :job_title, :website, :rating, :street_address, :city, :zip_code, :contact, :phone, :email, :job_posting_url, :resume_to_be_sent, :resume_actually_sent, :description, :follow_up, :challenge, :interview, :interview_info)
+      params.require(:jobby_job).permit(:company, :job_title, :website, :rating,
+        :street_address, :city, :zip_code, :contact, :phone, :email, :job_posting_url,
+        :resume_to_be_sent, :resume_actually_sent, :description, :follow_up, :challenge,
+        :interview, :interview_info)
     end
 end
